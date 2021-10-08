1fad682d211c299ac0e4a22ff8dc8aa226d29f44
Jordan <jordan.d.carlton@gmail.com>
2021-10-08 21:15:50 +0100

    created knight tests

--- Chessington.GameEngine.Tests/Pieces/KnightTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KnightTests.cs
@@ -1,9 +1,73 @@
-﻿using NUnit.Framework;
+﻿using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class KnightTests
     {
+        [Test]
+        public void Knight_CanMoveLShape()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            board.AddPiece(Square.At(0, 1), knight);
+
+            var moves = knight.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(2, 2));
+            moves.Should().Contain(Square.At(2, 0));
+        }
+        
+        [Test]
+        public void Knight_CanJumpOverPiece()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            var blocker = new Rook(Player.White);
+            var blocker2 = new Rook(Player.White);
+            var blocker3 = new Rook(Player.White);
+
+            board.AddPiece(Square.At(0, 1), knight);
+            board.AddPiece(Square.At(1, 0), blocker);
+            board.AddPiece(Square.At(1, 1), blocker2);
+            board.AddPiece(Square.At(1, 2), blocker3);
+
+            var moves = knight.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(2, 2));
+            moves.Should().Contain(Square.At(2, 0));
+        }
+        
+        [Test]
+        public void Knight_CanCaptureEnemyPiece()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            var enemy = new Rook(Player.Black);
+
+            board.AddPiece(Square.At(3, 3), knight);
+            board.AddPiece(Square.At(4, 5), enemy);
+
+
+            var moves = knight.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(4, 5));
+        }
+        
+        [Test]
+        public void Knight_CantMoveToSelf()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+
+            board.AddPiece(Square.At(0, 1), knight);
+
+            var moves = knight.GetAvailableMoves(board);
+
+            moves.Should().NotContain(Square.At(0, 1));
+
+        }
     }
 }
--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,30 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            //Initialise variables
+            var currentSquare = board.FindPiece(this);
+            var knightMoves = new List<Square>();
+            
+            //Possible moves
+            int []X = { 2, 1, -1, -2, -2, -1, 1, 2 };
+            int []Y = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            int count = 0;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                //new position
+                int x = currentSquare.Row + X[i];
+                int y = currentSquare.Col + Y[i];
+                
+                //if move is valid, add to move list
+            }
+
+
+            return knightMoves;
+            // L shaped moves
+            // Can jump over pieces
+            // Capture by landing on piece
         }
     }
 }
