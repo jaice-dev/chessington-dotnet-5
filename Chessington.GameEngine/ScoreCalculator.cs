9612386af8d5794e4098f5b67819690e129dbe3b
Jordan <jordan.d.carlton@gmail.com>
2021-10-11 10:57:40 +0100

    init ScoreCalculator.cs

--- Chessington.GameEngine/ScoreCalculator.cs
+++ Chessington.GameEngine/ScoreCalculator.cs
@@ -2,6 +2,23 @@
 {
     public class ScoreCalculator
     {
-        
+        private Board _board;
+
+        public ScoreCalculator(Board board)
+        {
+            _board = board;
+        }
+
+        public int GetWhiteScore()
+        {
+            // Should add up the value of all of the pieces that white has taken.
+            return 0;
+        }
+
+        public int GetBlackScore()
+        {
+            // Should add up the value of all of the pieces that black has taken.
+            return 0;
+        }
     }
 }
